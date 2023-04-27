707e184c46ba47e7d126bb6ed0ea11033ec25896
Nicolas Sadin <5952290+SlevinWasAlreadyTaken@users.noreply.github.com>
2023-04-27 18:02:28 +0200

    minor FIX existing-pr non detected by using PerPage instead of Page on PR listing, close all http responses (#5)
    * add debug for bundler
    
    * print number of found PRs
    
    * print body of pr srequest resp
    
    * print correctly the body..
    
    * add even more debug
    
    * get all pr instead of open only to see results
    
    * use PerPage and not Page for PR listing
    
    * revert some debugging
    
    * remove unecessary white space

--- internal/github/client.go
+++ internal/github/client.go
@@ -26,10 +26,11 @@ func NewClient(ctx context.Context, ghToken string) (Client, error) {
 
 // GetAuthenticatedUsername return the username of the current authenticated user.
 func (c Client) GetAuthenticatedUsername(ctx context.Context) (string, error) {
-	user, _, err := c.Client.Users.Get(ctx, "") // empty string makes the library returning the authenticated user
+	user, resp, err := c.Client.Users.Get(ctx, "") // empty string makes the library returning the authenticated user
 	if err != nil {
 		return "", fmt.Errorf("getting user: %v", err)
 	}
+	defer resp.Body.Close()
 	if user == nil {
 		return "", fmt.Errorf("retrieved a nil user")
 	}
@@ -42,12 +43,14 @@ func (c Client) GetAuthenticatedUsername(ctx context.Context) (string, error) {
 // GetHeadBranchNameByPRNumbers for a given repository as a map. Consider only opened PRs.
 func (c Client) GetHeadBranchNameByPRNumbers(ctx context.Context, owner, repoName string) (map[int]string, error) {
 	// max page size is 100: https://docs.github.com/en/rest/pulls/pulls?apiVersion=2022-11-28#list-pull-requests
-	opt := &github.PullRequestListOptions{State: "open", ListOptions: github.ListOptions{Page: 99}}
+	opt := &github.PullRequestListOptions{State: "open", ListOptions: github.ListOptions{PerPage: 99}}
 
-	prs, _, err := c.Client.PullRequests.List(ctx, owner, repoName, opt)
+	prs, resp, err := c.Client.PullRequests.List(ctx, owner, repoName, opt)
 	if err != nil {
 		return nil, fmt.Errorf("listing prs: %v", err)
 	}
+	defer resp.Body.Close()
+
 	// log a warning if the number of PR retrieved is the maximum page size
 	if len(prs) == 99 { //nolint:gomnd
 		log.Warnf("99 opened PRs on this repository, this may make the synchronization to fail")
@@ -79,19 +82,21 @@ func (c Client) CreateOrUpdatePR(
 			Body:                &desc,
 			MaintainerCanModify: &canBeModified,
 		}
-		createdPR, _, err := c.Client.PullRequests.Create(ctx, owner, repoName, pr)
+		createdPR, resp, err := c.Client.PullRequests.Create(ctx, owner, repoName, pr)
 		if err != nil {
 			return fmt.Errorf("creating PR: %s", err)
 		}
+		defer resp.Body.Close()
 		log.Infof("PR created: %s", *createdPR.HTMLURL)
 	} else { // update mode = create a comment with the given desc
 		desc = fmt.Sprintf("PR updated with additional changes: %s", desc)
-		prComment, _, err := c.Client.Issues.CreateComment(ctx, owner, repoName, *existingPRNumber, &github.IssueComment{
+		prComment, resp, err := c.Client.Issues.CreateComment(ctx, owner, repoName, *existingPRNumber, &github.IssueComment{
 			Body: &desc,
 		})
 		if err != nil {
 			return fmt.Errorf("creating comment on PR: %v", err)
 		}
+		defer resp.Body.Close()
 		log.Infof("PR updated: %s", *prComment.HTMLURL)
 	}
 	return nil
